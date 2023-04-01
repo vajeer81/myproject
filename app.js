ecc3d3eb85483b4841bb358a88bd324a4e93e9b0
vajeer81 <vajeermohammad81@gmail.com>
2023-04-01 15:27:15 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require("mongoose")
 app.use(express.json())
 const artical = require("./Router/articalrouter")
 app.use("/api/artical",artical)
-
+mongoose.connect("mongodb+srv://vajeermohammad81:vajeerkhan123@cluster0.uoobtbm.mongodb.net/test")
+// mongoose.connect('mongodb+srv://vk:vajeerkhan@cluster0.ibpgv3a.mongodb.net/test')
+mongoose.connection.on('error', err => {
+    console.log('================>database failed ') 
+})
+mongoose.connection.on('connected', connected => { 
+    console.log('=====================>database connected')
+})
 
 
 app.listen(3000)
