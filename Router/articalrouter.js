563755525007d8cf0486502b8a281f6f32871b44
vajeer81 <vajeermohammad81@gmail.com>
2023-04-02 17:20:51 +0530

    first commit

--- Router/articalrouter.js
+++ Router/articalrouter.js
@@ -1,4 +1,5 @@
 const artical = require("../model/articalmodel")
+const jwt = require("jsonwebtoken")
 const express = require("express")
 const router = express.Router()
 router.use(express.json())
@@ -22,10 +23,39 @@ router.post("/", async (req, res) => {
             dis,
             image
         })
-        res.status(200).json(data)
+        res.status(200).json({
+            title:data.title,
+            dis:data.dis,
+            image:data.image,
+            token:tokenjanret(data._id)
+        
+        })
     } catch (error) {
         res.status(404).json(error)
     }
 })
 
+
+router.put("/:_id",async(req,res)=>{
+    try {
+       const data = await artical.findById(req.params._id)
+       const update = await artical.findOneAndUpdate(req.params._id,req,body,{new:true}) 
+       res.status(200).json(update)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+router.delete("/:_id",async(req,res)=>{
+    try {
+        const deleteresult = await artical.deleteMany({_id:req.params._id})
+        res.status(200).json({deleteresult:deleteresult})
+    } catch (error) {
+        res.status(404).json(error)
+        
+    }
+})
+
+const tokenjanret = (id)=>{
+    return jwt.sign({id},"vajeerkhan",{expiresIn:"30d"})
+}
 module.exports = router
--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.use(express.json())
 const artical = require("./Router/articalrouter")
 app.use("/api/artical",artical)
 mongoose.connect("mongodb+srv://vajeermohammad81:vajeerkhan123@cluster0.uoobtbm.mongodb.net/test")
-// mongoose.connect('mongodb+srv://vk:vajeerkhan@cluster0.ibpgv3a.mongodb.net/test')
 mongoose.connection.on('error', err => {
     console.log('================>database failed ') 
 })
